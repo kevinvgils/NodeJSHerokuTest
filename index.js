9a76fdd9ea4223c9b25a158abd7649a571f1bd66
Kevin van Gils <kvnvangils@gmail.com>
2022-04-24 16:15:28 +0200

    fixed everything and removed stuff

--- index.js
+++ index.js
@@ -1,48 +1,122 @@
     const express = require('express')
     const app = express()
     const port = process.env.PORT || 3000
+    const bodyParser = require('body-parser')
+
+    app.use(bodyParser.json())
+
+    let database = [];
+    let id = 0;
 
     app.get('/', (req, res) => {
-    res.send('Hello World!')
+        res.status(200).json({
+            status: 200,
+            result: "Hello, World!"
+        })
     })
 
     // Make account for users
-    app.post('/api/user', (req, res) => {
-        res.send('Account made successfully')
+    app.route('/api/user')
+    .post((req, res) => {
+        let user = req.body
+        if (database.filter(item => item.emailAdress === req.body.emailAdress).length > 0) {
+            res.status(401).json({
+                status: 401,
+                message: req.body.emailAdress + " already exists!"
+            })
+        } else {
+            id++;
+            user = {
+                id,
+                ...user
+            }
+            database.push(user);
+            res.status(201).json({
+                status: 201,
+                result: user
+            })
+        }
     })
 
     // Get all users
-    app.get('/api/user', (req, res) => {
-        res.send('Get all users')
+    .get((req, res) => {
+        res.status(200).json({
+            status: 200,
+            result: database
+        })
     })
 
     // Requests own user profile
     app.get('/api/user/profile', (req, res) => {
-        res.send({
-            "id": 0,
-            "firstName": "John",
-            "lastName": "Doe",
-            "street": "Lovensdijkstraat 61",
-            "city": "Breda",
-            "isActive": true,
-            "emailAdress": "j.doe@server.com",
-            "password": "secret",
-            "phoneNumber": "06 12425475"
-          })
+        res.send('Not realized yet.')
     })
 
-    app.route('/api/user/:id')
+    app.route('/api/user/:userId')
     // Get single user by id
     .get((req, res) => {
-        res.send('Get user by id')
+        const userId = parseInt(req.params.userId);
+
+        let user = database.filter((item) => item.id === userId);
+
+        if (user.length > 0) {
+            res.status(200).json({
+                status: 200,
+                result: user
+            })
+        } else {
+            res.status(404).json({
+                status: 404,
+                message: 'User not found'
+            })
+        }
     })
     // Update single user by id
-    .post((req, res) => {
-        res.send('Updated user by id')
+    .put((req, res) => {
+        const userId = parseInt(req.params.userId);
+        let updatedUser = req.body
+
+        // Vind index van user object in database (array)
+        index = database.findIndex((item => item.id === userId));
+
+        if(index != -1 && !database.filter(item => item.emailAdress === req.body.emailAdress && item.id !== userId).length > 0) {
+            database[index] = {
+                id: userId,
+                ...updatedUser
+            }
+
+            res.status(200).json({
+                status: 200,
+                result: database[index]
+            })
+        } else {
+            res.status(400).json({
+                status: 400,
+                message: 'User not found or email address already in use!'
+            })
+        }
     })
+
     // Delete single user by id
     .delete((req, res) => {
-        res.send('Deleted user by id')
+        const userId = parseInt(req.params.userId);
+
+        // Vind index van user object in database (array)
+        index = database.findIndex((item => item.id === userId));
+
+        if(index != -1) {
+            database.splice(index, 1)
+
+            res.status(200).json({
+                status: 200,
+                message: 'Deleted user successfully',
+                result: database
+            })
+        } else {
+            res.status(400).json({
+                status: 400,
+                message: 'User not found!'
+            })
+        }
     })
 
     app.listen(port, () => {
@@ -50,10 +124,16 @@
     })
 
     app.use((req, res, next) => {
-        res.status(404).send("Sorry can't find that!")
+        res.status(404).json({
+            status: 404,
+            message: "Sorry can't find that!"
+        })
     })
 
     app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
+        console.error(err.stack)
+        res.status(500).send({
+            status: 500,
+            message: err.message
+        })
     })
